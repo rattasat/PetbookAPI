847b5b9c69d24a66ad5e8ef9b242451568757932
Rattasat <rattasat.rtskt@gmail.com>
2018-01-29 22:47:40 +0700

    update mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,13 @@
 var config = require('./config');
 var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 
 module.exports = function () {
     mongoose.set('debug', config.debug);
-    var db = mongoose.connect(config.mongoUri, {
-        useMongoClient: true
-    });
-
+    mongoose.connect(config.mongoUri);
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
     require('../server/models/user.model');
     require('../server/models/pet.model');
     require('../server/models/petinlocation.model');
-
     return db;
-
 };
