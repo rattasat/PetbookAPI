b8e8c0054e6f1290aa13ec87d535fc530bdafe35
Rattasat <rattasat.rtskt@gmail.com>
2018-01-30 02:59:27 +0700

    30/01/61

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,10 +4,13 @@ var mongoose = require('mongoose');
 module.exports = function () {
     mongoose.set('debug', config.debug);
     mongoose.connect(config.mongoUri);
+
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
     require('../server/models/user.model');
     require('../server/models/pet.model');
     require('../server/models/petinlocation.model');
+    
     return db;
 };
--- server.js
+++ server.js
@@ -2,42 +2,18 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-// var mongoose = require('./config/mongoose');
-var mongoose = require('mongoose');
+var mongoose = require('./config/mongoose');
 var express = require('./config/express');
 var passport = require('./config/passport');
 var config = require('./config/config');
 
-module.exports = app;
-
-// var db = mongoose();
+var db = mongoose();
 var app = express();
 var passport = passport();
 
+var server = app.listen(process.env.PORT || 8081, function () {
+    var port = server.address().port;
+    console.log('server is running.. at port: %s', port);
+});
 
-
-connect()
-    .on(err, console.log)
-    .on('disconnected', connect)
-    .once('open', listen);
-
-function listen() {
-    app.listen(process.env.PORT || 8081, function () {
-        var port = server.address().port;
-        console.log('server is running.. at port: %s', port);
-    });
-}
-
-function connect() {
-    var options = {
-        useMongoClient: true
-    };
-
-    var db = mongoose.set('debug', config.debug);
-
-    require('./server/models/user.model');
-    require('./server/models/pet.model');
-    require('./server/models/petinlocation.model');
-
-    return db;
-}
+module.exports = app;
