ffa69b2202f7a1be1012babc3777eb090677e903
rattasat <rattasat.rtskt@gmail.com>
2018-03-05 02:34:02 +0700

    welcome message

--- server/controllers/index.controller.js
+++ server/controllers/index.controller.js
@@ -1,26 +1,3 @@
-var Pet = require('mongoose').model('Pet');
-
-exports.render = function(req, res) {
-    if (req.user) {
-        Pet.find({
-            username: req.user.username
-        }, '_id name', 
-        function(err, pets) {
-            if (err) {
-                next(err);
-            }
-            else {
-                res.render('index', {
-                    title: 'Petbook',
-                    username: req.user.firstName,
-                    lineStatus: req.user.lineStatus,
-                    verifyCode: req.user.verifyCode,
-                    pets: pets
-                });
-            }
-        });
-    }
-    else {
-        res.redirect('/login');
-    }
-};
+exports.welcome = function (req, res) {
+    res.send('welcome to petbook api');
+}
--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,5 +1,5 @@
 var index = require('../controllers/index.controller');
 
 module.exports = function(app) {
-    app.get('/', index.render);
-};
+    app.get('/', index.welcome);
+}
