e3e32d4f7b67b1b56ef0dd61b2ca0fba175b0eb9
rattasat <rattasat.rtskt@gmail.com>
2018-03-29 04:06:29 +0700

    config production

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 module.exports = {
-    mongoUri: 'mongodb://admin:password@ds047335.mlab.com:47335/petbookthai',
+    mongoUri: 'mongodb://admin:admin@ds127129.mlab.com:27129/petbookthai-prod',
     secret: 'prod_secret_key',
-    accessToken: 'BkhlVtYNhUmIigiQ2/IADl3bwkke3+j3UdYuIpjoRl5FILv5LCt97MuhxPewvm+WvY3ew2n5vVq2NK/g1VTG0fqbQebUVrouCZ3Ek6zh0xRRCs1GVpvq1Gmcn3QMRTgflBSjm2lNjd6Ft895m7kpYgdB04t89/1O/w1cDnyilFU=',
-    channelSecret: 'cafedf4c8c30c770400dc2d8686afdcb',
+    accessToken: 'A2+YP0WfdPZeA+SV14BYNctZMLP2outdB5FLsD5fa5kMAOsqfF6478u68dYlWvJAsiBIWf/ZnczrEXZ5GJdQHjkBq5KHMVqJACTa/l96j9RT8CFYA+usJokWFzGkYA0/L8SDJAiQxPzt9rL4cR04wQdB04t89/1O/w1cDnyilFU=',
+    channelSecret: 'fb0e51215aaf20331b6302c60672e867',
     debug: false
 };
