97d666d1a3d0ae19c8f8332a14f1477a918191bf
Rattasat Kruetong <rattasat.kru@ascendcorp.com>
2018-08-16 23:08:29 +0700

    change config

--- config/env/development.js
+++ config/env/development.js
@@ -3,6 +3,5 @@ module.exports = {
     secret: 'dev_secret_key',
     accessToken: 'BkhlVtYNhUmIigiQ2/IADl3bwkke3+j3UdYuIpjoRl5FILv5LCt97MuhxPewvm+WvY3ew2n5vVq2NK/g1VTG0fqbQebUVrouCZ3Ek6zh0xRRCs1GVpvq1Gmcn3QMRTgflBSjm2lNjd6Ft895m7kpYgdB04t89/1O/w1cDnyilFU=',
     channelSecret: 'cafedf4c8c30c770400dc2d8686afdcb',
-    url: 'http://localhost:4200',
     debug: true
 };
--- server.js
+++ server.js
@@ -2,16 +2,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose');
 var express = require('./config/express');
-var config = require('./config/config');
 
 var db = mongoose();
 var app = express();
-
-// var http = require("http");
-// setInterval(function () {
-//     http.get("http://petbookthaiapi.herokuapp.com");
-//     // http.get("http://localhost:8081/");
-// }, 300000); // every 5 minutes (300000)
 
 var server = app.listen(process.env.PORT || 8081, function () {
     var port = server.address().port;
